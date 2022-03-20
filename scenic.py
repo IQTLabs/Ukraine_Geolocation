5d6133bced3b7a47cbe02cadd9d863edf6e63930
Daniel Hogan <6313241+dphogan@users.noreply.github.com>
2022-03-20 11:56:02 -0400

    Partway through moving transform to its own function

--- scenic.py
+++ scenic.py
@@ -5,11 +5,11 @@ import torchvision
 import os
 from PIL import Image
 
-def load_surface_model(arch='alexnet'):
+def load_model(arch='alexnet', view='surface'):
     """
     Based on https://github.com/CSAILVision/places365/blob/master/run_placesCNN_basic.py by Bolei Zhou
     """
-    model_path = '../weights/%s_places365.pth.tar' % arch
+    model_path = '../weights/%s_%s.pth.tar' % (arch, view)
     model = torchvision.models.__dict__[arch](num_classes=365)
     checkpoint = torch.load(model_path, map_location=lambda storage, loc: storage)
     state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
@@ -17,8 +17,34 @@ def load_surface_model(arch='alexnet'):
     model.eval()
     return model
 
+def get_transform(view='surface', preprocess=True, finalprocess=True):
+    """
+    
+    """
+    transform_list = []
+    if preprocess:
+        if view == 'surface':
+            transform_list.extend([
+                torchvision.transforms.Resize((256,256)),
+                torchvision.transforms.CenterCrop(224),
+            ])
+        elif view == 'overhead':
+            transform_list.append(
+                torchvision.transforms.Resize((224,224))
+            )
+        else:
+            raise Exception('! Invalid view in ' + __name__)
+    if finalprocess:
+        transform_list.extend([
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize([0.485, 0.456, 0.406],
+                                             [0.229, 0.224, 0.225])
+        ])
+    transform = torchvision.transform.Compose(transform_list)
+    return transform
 
-def save_surface_features():
+
+def save_features(overhead=False):
 
     surface_transform = torchvision.transforms.Compose([
         torchvision.transforms.Resize((256,256)),
@@ -27,8 +53,16 @@ def save_surface_features():
         torchvision.transforms.Normalize([0.485, 0.456, 0.406],
                                          [0.229, 0.224, 0.225])
     ])
+    overhead_transform = torchvision.transforms.Compose([
+        torchvision.transforms.Resize((224,224)),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize([0.485, 0.456, 0.406],
+                                         [0.229, 0.224, 0.225])
+    ])
+    transform = overhead_transform if overhead else surface_transform
 
-    model = load_surface_model()
+    name = 'cvplaces' if overhead else 'places'
+    model = load_model(view=name)
 
     img = Image.open('../example/60949863@N02_7984662477_43.533763_-89.290620.jpg')
     batch = torch.autograd.Variable(surface_transform(img).unsqueeze(0))
@@ -39,4 +73,4 @@ def save_surface_features():
 
 
 if __name__ == '__main__':
-    save_surface_features()
+    save_features()
