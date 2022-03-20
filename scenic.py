add5d6e7a17b739370ab877f69469ac4521f8759
Daniel Hogan <6313241+dphogan@users.noreply.github.com>
2022-03-20 16:39:47 -0400

    Data loader that generates paths on each call

--- scenic.py
+++ scenic.py
@@ -1,9 +1,68 @@
 #!/usr/bin/env python
 
+import os
+import re
 import torch
 import torchvision
-import os
+import pandas as pd
 from PIL import Image
+
+
+class OneDataset(torch.utils.data.Dataset):
+    """
+    Dataset for a single view (surface or overhead)
+    Input file format is:
+    path[,latitude,longitude[,feature_vector_components]]
+    where brackets denote optional entries
+    """
+    def __init__(self, input_file, view='surface', zoom=18, rule='cvusa', transform=None):
+        self.input_file = input_file
+        self.view = view # surface, overhead
+        self.zoom = zoom # 18, 16, 14
+        self.rule = rule # cvusa, literal
+        self.transform = transform
+        self.df = pd.read_csv(
+            self.input_file, header=None,
+            names=['path', 'lat', 'lon']
+        )
+    def __len__(self):
+        return len(self.df)
+    def __getitem__(self, idx):
+        data = {}
+        data['path_csv'] = os.path.split(self.input_file)[0]
+        data['path_listed'] = self.df.iloc[idx]['path']
+        data['path_relative'] = data['path_listed']
+        if self.view == 'overhead' and self.rule == 'cvusa':
+            # Convert CVUSA streetview surface path to overhead path
+            data['path_relative'] = data['path_relative'].replace(
+                'streetview/cutouts', 'streetview_aerial/' + str(self.zoom), 1)
+            data['path_relative'] = data['path_relative'].replace(
+                '_90.jpg', '.jpg', 1)
+            data['path_relative'] = data['path_relative'].replace(
+                '_270.jpg', '.jpg', 1)
+            # Convert CVUSA flickr surface path to overhead path
+            data['path_relative'] = data['path_relative'].replace(
+                'flickr', 'flickr_aerial/' + str(self.zoom), 1)
+            data['path_relative'] = re.sub(
+                r'[0-9]+@N[0-9]+_[0-9]+_', '', data['path_relative'])
+
+        return data
+    def populate_latlon(self):
+        """
+        Populate latitude and longitude columns from path column.
+        This only works if entries follow format of CVUSA dataset.
+        """
+        def select_fields(s):
+            l = s.split('_')
+            if len(l) == 3:
+                return '_'.join(l[:2])
+            if len(l) == 4:
+                return '_'.join(l[2:])
+            else:
+                raise Exception('! Invalid string in ' + __name__)
+        names = self.df['path'].apply(lambda x: os.path.splitext(os.path.split(x)[1])[0])
+        strings = names.apply(select_fields)
+        self.df[['lat', 'lon']] = strings.str.split('_', expand=True)
 
 
 def get_transform(view='surface', preprocess=True, finalprocess=True):
@@ -46,6 +105,16 @@ def load_model(view='surface', arch='alexnet'):
     return model
 
 
+def preprocess(input_file, output_dir, view='surface'):
+
+    transform = get_transform(view, preprocess=True, finalprocess=False)
+    dataset = OneDataset(input_file, view=view, transform=transform)
+    dataset.populate_latlon()
+
+    for idx in range(len(dataset)):
+        print(dataset[idx])
+
+
 def save_features(view='surface'):
 
     transform = get_transform(view)
@@ -61,4 +130,6 @@ def save_features(view='surface'):
 
 
 if __name__ == '__main__':
-    save_features()
+    preprocess('/local_data/crossviewusa/streetview_images.txt', '../temp', view='overhead')
+    #preprocess('/local_data/crossviewusa/sample/streetview_images.txt', '../temp', view='overhead')
+    #save_features()
