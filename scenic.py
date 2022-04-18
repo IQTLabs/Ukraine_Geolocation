59a10acf4555898833cc0be2586939aca32c8c48
Daniel Hogan <6313241+dphogan@users.noreply.github.com>
2022-04-18 15:15:09 -0400

    Change column names in example_features()

--- scenic.py
+++ scenic.py
@@ -449,13 +449,13 @@ def example_features(path, view='surface', verbose=True,
     probs = torch.nn.functional.softmax(logits, 0)
     if verbose:
         df = pd.read_csv(names_path, sep=' ', header=None,
-                         names=['scenes'], usecols=[0])
-        df['probs'] = probs.numpy()
-        df.sort_values('probs', ascending=False, inplace=True)
+                         names=['scene'], usecols=[0])
+        df['prob'] = probs.numpy()
+        df.sort_values('prob', ascending=False, inplace=True)
         df.reset_index(drop=True, inplace=True)
         print(path)
         print(df[:5])
-    return logits.squeeze()
+    return logits
 
 
 class Translator(object):
