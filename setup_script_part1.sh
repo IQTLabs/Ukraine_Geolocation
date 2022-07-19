eaa3a8970a09a35aedbc25787cc93538a5f21eee
Daniel Hogan <6313241+dphogan@users.noreply.github.com>
2022-07-19 16:15:57 -0400

    Fix download commands

--- setup_script_part1.sh
+++ setup_script_part1.sh
@@ -5,10 +5,11 @@ mkdir data
 mkdir weights
 
 # Places2 weights
-wget http://places2.csail.mit.edu/models_places365/alexnet_places365.pth.tar weights
+wget http://places2.csail.mit.edu/models_places365/alexnet_places365.pth.tar -O weights/alexnet_surface.pth.tar
+wget http://places2.csail.mit.edu/models_places365/densenet161_places365.pth.tar -O weights/densenet161_surface.pth.tar
 
 # Code
-git clone https://github.com/IQTLabs/Ukraine_Geolocalization.git code
+git clone https://github.com/IQTLabs/Ukraine_Geolocation.git code
 cd code
 wget https://raw.githubusercontent.com/CSAILVision/places365/master/categories_places365.txt
 
